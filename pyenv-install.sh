5c0c378ad98e4af00ab6ef8ae1d06e7a132693ee
wade <wadechen@outlook.comm>
2020-02-18 23:16:11 +0900

    modify pyenv install

--- pyenv-install.sh
+++ pyenv-install.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
 # install dependency package
-yum install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel -y
+yum install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel xz-devel make -y
+# db4-devel libpcap-devel
 
 # download pyenv
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 
 # set environment parameters
 echo "export PYENV_HOME=$HOME/.pyenv" >> /etc/profile
+source /etc/profile
 echo "export PATH=$PATH:$PYENV_HOME/bin" >> /etc/profile
 echo 'eval "$(pyenv init -)"' >> /etc/profile
+source /etc/profile
 
 # rerun shell
-source /etc/profile
 exec $SHELL
