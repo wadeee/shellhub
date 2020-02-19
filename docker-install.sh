47489030c09115dc224e78f004ccec26e5efb49a
wade <wadechen@outlook.com>
2020-02-19 14:12:53 +0900

    modify docker install

---
+++ docker-install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+yum remove docker \
+           docker-client \
+           docker-client-latest \
+           docker-common \
+           docker-latest \
+           docker-latest-logrotate \
+           docker-logrotate \
+           docker-engine
+
+yum install -y yum-utils \
+               device-mapper-persistent-data \
+               lvm2
+
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+yum install docker-ce docker-ce-cli containerd.io
+
+systemctl start docker
