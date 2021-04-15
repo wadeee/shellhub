401bf84eec27c827531701420a210ea5439ef90b
wade <wadechen@outlook.com>
2021-04-15 17:18:04 +0800

    modify mysql-install.sh

--- mysql-install.sh
+++ mysql-install.sh
@@ -6,3 +6,10 @@ dnf -y install @mysql
 systemctl enable mysqld
 ## run the mysql server ##
 systemctl start mysqld
+
+## Myslq8 ##
+## update password ##
+# mysqladmin -u root -p password 'new password'
+## enable root remote login ##
+# use mysql;
+# update user set host='%' where user ='root';
