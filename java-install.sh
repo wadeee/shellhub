c1aa45dcf7a03fb61798b8667ee657fac031810b
Wade <wadechen@outlook.com>
2021-05-21 09:33:26 +0800

    modify java-install.sh

--- java-install.sh
+++ java-install.sh
@@ -5,7 +5,7 @@ dnf install -y java-1.8.0-openjdk-devel.x86_64
 
 ## set environment parameters ##
 ## important! this path will change please care ##
-echo "export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.282.b08-2.el8_3.x86_64" >> /etc/profile
+echo "export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.292.b10-0.el8_3.x86_64" >> /etc/profile
 source /etc/profile ## IMPOTANT! for next step ##
 echo "export CLASSPATH=.:$JAVA_HOME/jre/lib/rt.jar:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar" >> /etc/profile
 echo "export PATH=$PATH:$JAVA_HOME/bin" >> /etc/profile
