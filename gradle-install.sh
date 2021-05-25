266016489027ef8d5aa211b9bc2f00cf5902a0bb
Wade <wadechen@outlook.com>
2021-05-25 10:28:34 +0800

    modify sdkman-install.sh

--- gradle-install.sh
+++ gradle-install.sh
@@ -1,16 +1,4 @@
 #!/bin/sh
 
-## install zip and unzip ##
-dnf install -y zip
-dnf install -y unzip
-# reboot
-
-## install SDK man ##
-curl -s "https://get.sdkman.io" | bash
-source "$HOME/.sdkman/bin/sdkman-init.sh"
-# echo 'source "$HOME/.sdkman/bin/sdkman-init.sh"' >> ~/.zshrc
-
-# sdk version
-
 ## install gradle
 sdk install gradle
