375656201c2c1e2cc093a6ea054d092b1efdf9d0
Wade <wadechen@outlook.com>
2021-05-25 10:29:00 +0800

    modify sdkman-install.sh

---
+++ sdkman-install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+## install zip and unzip ##
+dnf install -y zip
+dnf install -y unzip
+# reboot
+
+## install SDK man ##
+curl -s "https://get.sdkman.io" | bash
+#source "$HOME/.sdkman/bin/sdkman-init.sh"
+# echo 'source "$HOME/.sdkman/bin/sdkman-init.sh"' >> ~/.zshrc
+
+# sdk version
+
+reboot
