7979714a54aa47f523f6b30bd9346622e1b044f9
Wade <wadechen@outlook.com>
2021-06-09 14:40:48 +0800

    modify sshkey.sh

--- sshkey.sh
+++ sshkey.sh
@@ -6,6 +6,7 @@ ssh-keygen -t rsa -C "youremail.com"
 ssh-keygen -m PEM -t rsa -C "youremail.com"
 ssh-keygen -m PEM -t rsa -C "youremail.com" -f ~/.ssh/id_rsa_ssh8
 
-scp /c/Users/TS/.ssh/id_rsa.pub root@192.168.0.91:~/.ssh/wade.pub
-
-cat wade.pub >>~/.ssh/authorized_keys
+## add public key to the server
+ssh-copy-id -i ~/.ssh/id_rsa_ssh8 root@192.168.0.91
+## scp /c/Users/TS/.ssh/id_rsa.pub root@192.168.0.91:~/.ssh/wade.pub
+## cat wade.pub >>~/.ssh/authorized_keys
