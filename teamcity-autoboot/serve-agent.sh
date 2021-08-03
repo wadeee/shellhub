2c62e6aa869d7f11723084ea022501837da634bc
Wade <wadechen@outlook.com>
2021-08-03 15:16:49 +0800

    teamcity

--- teamcity-autoboot/serve-agent.sh
+++ teamcity-autoboot/serve-agent.sh
@@ -7,7 +7,8 @@
 ## cd /var/local/buildagent/conf
 ## cp buildAgent.dist.properties buildAgent.properties
 ## vi buildAgent.properties ## set the name and serverUrl
+ssh -p 36000 -i C:/Users/TS/.ssh/id_rsa_ssh8 root@office.cellx.com.cn "wget http://localhost:36009/update/buildAgentFull.zip && unzip buildAgentFull.zip -d /var/local/buildagent"
 
 ## agent config
-scp -P 36010 -i C:/Users/wade/.ssh/id_rsa_ssh8 ./teamcity-agent.service root@office.cellx.com.cn:/usr/lib/systemd/system/
-ssh -p 36010 -i C:/Users/wade/.ssh/id_rsa_ssh8 root@office.cellx.com.cn "systemctl daemon-reload && systemctl enable teamcity-agent && systemctl restart teamcity-agent"
+scp -P 36000 -i C:/Users/TS/.ssh/id_rsa_ssh8 ./teamcity-agent.service root@office.cellx.com.cn:/usr/lib/systemd/system/
+ssh -p 36000 -i C:/Users/TS/.ssh/id_rsa_ssh8 root@office.cellx.com.cn "systemctl daemon-reload && systemctl enable teamcity-agent && systemctl restart teamcity-agent"
