7bf7388d575fa35e57587931579d525646bb03d8
wade <wadechen@outlook.com>
2022-10-19 11:36:58 +0800

    update

--- firewall.sh
+++ firewall.sh
@@ -16,5 +16,11 @@ firewall-cmd --add-port=36019/udp --zone=public --permanent
 ## reload
 firewall-cmd --reload
 
+## test tcp port
+firewall-cmd --query-port=36019/tcp
+
+## test udp port
+firewall-cmd --query-port=36019/udp
+
 ## remove port
 # firewall-cmd --remove-port=36019/tcp --zone=public --permanent
