6b2e32d2ab3967d88582067c87ec790a40d46c0d
wade <wadechen@outlook.com>
2022-12-21 18:05:09 +0800

    update

--- linux-normal.sh
+++ linux-normal.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
+## copy file
 cat a.txt
 cat a.txt > b.txt
 
+## move file
+mv a.txt b.txt
+
+## tail line real time
 tail -n 3 -f /var/log/dnf.log
 
+## get system arch info
 arch
 uname -a
 
+## rpm install
 rpm -ivh /root/software/redis-6.2.6-2.1.x86_64.rpm
 
 rpm -qa
