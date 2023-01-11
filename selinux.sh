002e8c95f4dd58e03dc55ed88ab2a6d976886151
wade <wadechen@outlook.com>
2023-01-11 17:05:14 +0800

    update

--- selinux.sh
+++ selinux.sh
@@ -22,8 +22,15 @@ getsebool -a | grep httpd_can_network_connect
 ## SELinux status
 sestatus -v
 
+## SELINUX临时状态设置
+setenforce permissive
+## enforcing   ## 强制模式（默认）
+## permissive  ## 宽容模式
+## disabled    ## 禁用
+
 ## SELINUX状态设置
 vi /etc/selinux/config
+reboot
 ## SELINUX=enforcing   ## 强制模式（默认）
 ## SELINUX=permissive  ## 宽容模式
 ## SELINUX=disabled    ## 禁用
