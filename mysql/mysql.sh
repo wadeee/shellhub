f602750f47b4c244ff2ad4b5206dbaff4f18bb8b
wade <wadechen@outlook.com>
2023-03-21 10:45:18 +0800

    update

--- mysql/mysql.sh
+++ mysql/mysql.sh
@@ -4,10 +4,8 @@
 mysqldump -uroot -ppassword -h192.168.0.90 --port=3306 dbname > C:/Users/Wadec/Desktop/target.sql
 
 ## mysql import
-mysql -uroot -ppassword -h192.168.0.90 --port=3306 dbname < C:/Users/Wadec/Desktop/cellx_base.sql
+mysql -uroot -ppassword -h192.168.0.90 --port=3306 dbname < C:/Users/Wadec/Desktop/target.sql
 
 ## run sql
-mysql -uroot -ppassword -h192.168.0.90 --port=3306 -s -e "source /root/sql/cellx_base.sql;"
-mysql -uroot -ppassword -h192.168.0.90 --port=3306 -s -e "use cellx_base; show schemas;"
-
-##
+mysql -uroot -ppassword -h192.168.0.90 --port=3306 -s -e "SOURCE /root/sql/target.sql;"
+mysql -uroot -ppassword -h192.168.0.90 --port=3306 -s -e "USE `dbname`; SHOW SCHEMAS;"
--- mysql/mysql.sql
+++ mysql/mysql.sql
@@ -7,6 +7,34 @@ DROP SCHEMA `dbname`;
 -- use databse
 USE `dbname`;
 
+-- create table
+CREATE TABLE `dbname` (
+  `col1` VARCHAR(255) NOT NULL,
+  `col2` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`col1`)
+);
+
+-- drop table
+DROP TABLE `dbname`;
+
+-- rename table
+RENAME TABLE `dbname` TO `new-dbname`;
+
+-- show schemas;
+SHOW SCHEMAS;
+
+-- add column
+ALTER TABLE `dbname` ADD COLUMN `col1` VARCHAR(255) NOT NULL;
+
+-- drop column
+ALTER TABLE `dbname` DROP COLUMN `col1`;
+
+-- rename column
+ALTER TABLE `dbname` CHANGE COLUMN `col1` `new-col1` VARCHAR(255) NOT NULL;
+
+-- import
+SOURCE /root/temp/target.sql;
+
 -- insert
 INSERT INTO `dbname` (col1, col2) VALUE ('1', '1');
 INSERT INTO `dbname` (col1, col2) VALUES ('1', '1'), ('2', '3');
@@ -16,6 +44,3 @@ UPDATE `dbname` SET col1='3', col2='3' WHERE col1='1';
 
 -- delete
 DELETE FROM `dbname` WHERE col1='1';
-
--- import
-SOURCE /root/temp/target.sql;
