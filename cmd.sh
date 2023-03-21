53bf989d09be3b61d3939426261b41a907e3b445
wade <wadechen@outlook.com>
2023-03-21 11:38:22 +0800

    update

--- cmd.sh
+++ cmd.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+## remove dir and all files
+rmdir /s /q "C:\Program Files\MySQL\MySQL Server 8.0\data"
+
 ## create 1mb empty file
 fsutil file createnew a.txt 1048576
 # or
 set /a var = 1024*1024
-fsutil file createnew C:/Users/Wadec/Desktop/a.txt %var%
-
+fsutil file createnew "C:/Users/Wadec/Desktop/a.txt" %var%
