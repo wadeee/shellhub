2fcab9177034455f56f985978eda890d3f9ee67b
Wade <wadechen@outlook.com>
2023-07-30 11:41:06 +0800

    update shell

--- sshkey.sh
+++ sshkey.sh
@@ -6,6 +6,9 @@ ssh-keygen -t rsa -C "youremail.com"
 ssh-keygen -m PEM -t rsa -C "youremail.com"
 ssh-keygen -m PEM -t rsa -C "youremail.com" -f ~/.ssh/id_rsa_ssh8
 
+## remove the host identification
+ssh-keygen -R 192.168.0.147
+
 ## add public key to the server
 ssh-copy-id -i ~/.ssh/id_rsa root@192.168.0.91
 ssh-copy-id -p 22 -i ~/.ssh/id_rsa root@192.168.0.91
