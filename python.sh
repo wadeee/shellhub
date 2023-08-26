8d09af37b81bcbfeba73fda1a7476fa5ddf0b8e6
Wade <wadechen@outlook.com>
2023-08-26 10:06:49 +0800

    python

--- python.sh
+++ python.sh
@@ -10,3 +10,18 @@ pip install -r requirements.txt
 
 ## protobuf
 protoc -I protobuf --python_out=protobuf protobuf/dy.proto
+
+## pyinstaller
+pyinstaller main.py
+pyinstaller --onefile main.py
+pyinstaller -F -w -i favicon.ico main.py
+
+## pyenv
+pyenv version # 查看当前系统的python版本
+pyenv versions # 查看当前系统安装的python版本
+pyenv install --list # 查看可以安装的python版本
+pyenv install 3.11.4 # 安装python3.11.4版本
+pyenv uninstall 3.11.4 # 卸载python3.11.4版本
+pyenv local 3.11.4 # 在当前目录下创建一个.python-version文件，内容为3.11.4，表示当前目录下的python版本为3.11.4
+pyenv global 3.11.4 # 设置全局python版本为3.11.4
+pyenv shell 3.11.4 # 设置当前shell的python版本为3.11.4
