afeb9f1cc900550058e2024bbddb7f811a544fb3
wade <wadechen@outlook.com>
2024-02-20 14:40:16 +0800

    update

--- mysql/mysql.sh
+++ mysql/mysql.sh
@@ -2,6 +2,7 @@
 
 ## mysqldump export
 mysqldump -uroot -ppassword -h192.168.0.90 --port=3306 dbname > C:/Users/Wadec/Desktop/target.sql
+mysqldump --result-file=C:/Users/Wadec/Desktop/target.sql --disable-keys --create-options --skip-extended-insert --routines dbname --user=root --host=10.188.30.170 --port=3306
 
 ## mysql import
 mysql -uroot -ppassword -h192.168.0.90 --port=3306 dbname < C:/Users/Wadec/Desktop/target.sql
