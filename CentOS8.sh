dda243af2cbe0b34cf17099ab925d3d46f997f8a
wade <wadechen@outlook.com>
2024-06-28 14:42:36 +0800

    add CentOS8

---
+++ CentOS8.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+## CentOS8 EOL update repos
+sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+sudo dnf clean all
+sudo dnf update
