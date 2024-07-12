35b98d7089f4fc5f923081d66eec8efa36333e7b
wade <wadechen@outlook.com>
2024-07-12 11:50:52 +0800

    update nvm

---
+++ nvm/auto-install.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+remote_host=
+remote_user="root"
+remote_port="22"
+ssh_key=~/.ssh/id_rsa
+
+showHelp() {
+  echo "Usage: sh ./inst.sh [OPTIONS]"
+  echo "Options:"
+  echo "  -h <ssh host>"
+  echo "  -p <ssh port>"
+  echo "  -u <ssh user>"
+  exit;
+}
+
+while getopts "h:p:u:" opt; do
+  case "$opt" in
+    h ) remote_host=$OPTARG ;;
+    p ) remote_port=$OPTARG ;;
+    u ) remote_user=$OPTARG ;;
+    ? ) showHelp ;;
+  esac
+done
+
+## install nvm ##
+#scp -P "$remote_port" -i $ssh_key ./install.sh "$remote_user"@"$remote_host":/root/
+ssh -p "$remote_port" -i $ssh_key "$remote_user"@"$remote_host" "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash"
+ssh -p "$remote_port" -i $ssh_key "$remote_user"@"$remote_host" "bash ./install.sh"
+ssh -p "$remote_port" -i $ssh_key "$remote_user"@"$remote_host" "echo 'export NVM_DIR=\"\$HOME/.nvm\"' >> ~/.bashrc"
+ssh -p "$remote_port" -i $ssh_key "$remote_user"@"$remote_host" "echo '[ -s \"\$NVM_DIR/nvm.sh\" ] && \. \"\$NVM_DIR/nvm.sh\"' >> ~/.bashrc"
+ssh -p "$remote_port" -i $ssh_key "$remote_user"@"$remote_host" "echo '[ -s \"\$NVM_DIR/bash_completion\" ] && \. \"$NVM_DIR/bash_completion\"' >> ~/.bashrc"
+#export NVM_DIR="$HOME/.nvm"
+#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
