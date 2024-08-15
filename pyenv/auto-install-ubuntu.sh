97647692203edb758b1bdcfc73a06deff1a9f9a1
wade <wadechen@outlook.com>
2024-08-15 14:20:19 +0800

    update pyenv

--- pyenv/auto-install-ubuntu.sh
+++ pyenv/auto-install-ubuntu.sh
@@ -25,7 +25,7 @@ done
 
 
 ## install pyenv
-ssh -p "$remote_port" -i $ssh_key "$remote_user"@"$remote_host" "apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl git"
+ssh -p "$remote_port" -i $ssh_key "$remote_user"@"$remote_host" "apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev git"
 ssh -p "$remote_port" -i $ssh_key "$remote_user"@"$remote_host" "git clone https://github.com/pyenv/pyenv.git ~/.pyenv"
 #ssh -p "$remote_port" -i $ssh_key "$remote_user"@"$remote_host" "git clone https://gitee.com/wadde/pyenv.git ~/.pyenv"
 ssh -p "$remote_port" -i $ssh_key "$remote_user"@"$remote_host" "echo 'export PYENV_ROOT=\"\$HOME/.pyenv\"' >> ~/.bashrc"
