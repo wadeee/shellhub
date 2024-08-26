bab065a4f431bcdb09265e489a2385d7b63a57ee
wade <wadechen@outlook.com>
2024-08-26 15:50:16 +0800

    update CentOS8

--- CentOS8.sh
+++ CentOS8.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ## CentOS8 EOL update repos
-sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
-sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
-sudo dnf clean all
-sudo dnf update
+sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+dnf clean all
+dnf update
