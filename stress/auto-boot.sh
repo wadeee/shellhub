d612be1752e2fe082be877576c732245ea0e40c3
wade <wadechen@outlook.com>
2024-08-27 18:26:15 +0800

    update shell

---
+++ stress/auto-boot.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+remote_host=
+remote_user="root"
+remote_port="22"
+ssh_key=~/.ssh/id_rsa
+
+showHelp() {
+  echo "Usage: sh ./inst.sh [OPTIONS]"
+  echo "Options:"
+  echo "  -h <ssh host>"
+  echo "  -p <ssh port>"
+  echo "  -u <ssh user>"
+  exit;
+}
+
+while getopts "h:p:u:" opt; do
+  case "$opt" in
+    h ) remote_host=$OPTARG ;;
+    p ) remote_port=$OPTARG ;;
+    u ) remote_user=$OPTARG ;;
+    ? ) showHelp ;;
+  esac
+done
+
+## template
+# lscpu
+# nproc
+ssh -p "$remote_port" -i $ssh_key "$remote_user"@"$remote_host" "nohup stress --cpu 6 > /dev/null 2>&1 &"
