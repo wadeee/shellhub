aa4017bd1245169e7e99eeca3f7d5df9d6ed75db
wade <wadechen@outlook.com>
2024-09-26 15:38:27 +0800

    update mysql

--- MySQL/mysql.sh
+++ MySQL/mysql.sh
@@ -13,3 +13,8 @@ mysql -uroot -ppassword -h192.168.0.90 --port=3306 -s -e "USE `dbname`; SHOW SCH
 
 ## mysql secure installation
 mysql_secure_installation
+
+## mysql binlog
+mysqlbinlog --no-defaults --start-datetime="2024-09-26 15:00:00" --stop-datetime="2024-09-26 15:01:00" binlog.000001 | grep tablename
+mysqlbinlog --stop-position=11256359 binlog.000001 | mysql -uroot -p
+mysqlbinlog --stop-position=11256359 binlog.000001 | mysql -uroot -p --force
