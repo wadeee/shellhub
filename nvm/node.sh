b0493767b3c672336a17471625fbe8e53c2553c0
wade <wadechen@outlook.com>
2025-02-08 11:50:55 +0800

    update

--- nvm/node.sh
+++ nvm/node.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 npm install --registry=https://mirrors.huaweicloud.com/repository/npm/
+npm config set proxy http://localhost:7890
+npm config set https-proxy http://localhost:7890
