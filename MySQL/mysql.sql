df8401aaa1f9ae83700d13cfd3e99912c60cadef
wade <wadechen@outlook.com>
2025-04-18 10:45:52 +0800

    update

--- MySQL/mysql.sql
+++ MySQL/mysql.sql
@@ -1,4 +1,5 @@
 -- update low policy password
+-- cat /var/log/mysqld.log
 ALTER USER USER() IDENTIFIED BY "Password.1";
 SHOW VARIABLES LIKE 'validate_password%';
 SET GLOBAL validate_password.policy = LOW;
