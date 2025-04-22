dbb6d39395995e073b4b863ef345da47d031ef4a
wade <wadechen@outlook.com>
2025-04-22 16:22:02 +0800

    update

--- gogs/gogs-backup.sh
+++ gogs/gogs-backup.sh
@@ -6,3 +6,4 @@ cd /home/git || exit
 zip -r /mnt/data/home-git.zip ./*
 cd /root/gogs || exit
 zip -r /mnt/data/root-gogs.zip ./*
+mysqldump -uroot -ppassword --disable-keys --create-options --skip-extended-insert --routines --events --triggers gogs > /mnt/data/gogs.sql
