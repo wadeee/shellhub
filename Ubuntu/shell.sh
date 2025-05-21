58069c9b0e1c27218b9b3344b7609eca01dc11b6
wade <wadechen@outlook.com>
2025-05-21 13:41:22 +0800

    update

--- Ubuntu/shell.sh
+++ Ubuntu/shell.sh
@@ -1,3 +1,4 @@
+## enable root login
 ## modify /etc/ssh/sshd_config
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 sed -i '/^#PermitRootLogin prohibit-password$/a PermitRootLogin yes' /etc/ssh/sshd_config
