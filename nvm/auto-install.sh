21ad52b3a0038314b1a9d1a1ee441f4977aab8b3
wade <wadechen@outlook.com>
2025-05-21 17:24:45 +0800

    update

--- nvm/auto-install.sh
+++ nvm/auto-install.sh
@@ -25,11 +25,12 @@ done
 
 ## install nvm ##
 #scp -P "$remote_port" -i $ssh_key ./install.sh "$remote_user"@"$remote_host":/root/
-ssh -p "$remote_port" -i $ssh_key "$remote_user"@"$remote_host" "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash"
-ssh -p "$remote_port" -i $ssh_key "$remote_user"@"$remote_host" "bash ./install.sh"
-ssh -p "$remote_port" -i $ssh_key "$remote_user"@"$remote_host" "echo 'export NVM_DIR=\"\$HOME/.nvm\"' >> ~/.bashrc"
-ssh -p "$remote_port" -i $ssh_key "$remote_user"@"$remote_host" "echo '[ -s \"\$NVM_DIR/nvm.sh\" ] && \. \"\$NVM_DIR/nvm.sh\"' >> ~/.bashrc"
-ssh -p "$remote_port" -i $ssh_key "$remote_user"@"$remote_host" "echo '[ -s \"\$NVM_DIR/bash_completion\" ] && \. \"$NVM_DIR/bash_completion\"' >> ~/.bashrc"
+ssh -p "$remote_port" -i $ssh_key "$remote_user"@"$remote_host" "source /etc/profile.d/proxy.sh && proxy_on && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash"
+#ssh -p "$remote_port" -i $ssh_key "$remote_user"@"$remote_host" "bash ./install.sh"
+#ssh -p "$remote_port" -i $ssh_key "$remote_user"@"$remote_host" "echo 'export NVM_DIR=\"\$HOME/.nvm\"' >> ~/.bashrc"
+#ssh -p "$remote_port" -i $ssh_key "$remote_user"@"$remote_host" "echo '[ -s \"\$NVM_DIR/nvm.sh\" ] && \. \"\$NVM_DIR/nvm.sh\"' >> ~/.bashrc"
+#ssh -p "$remote_port" -i $ssh_key "$remote_user"@"$remote_host" "echo '[ -s \"\$NVM_DIR/bash_completion\" ] && \. \"\$NVM_DIR/bash_completion\"' >> ~/.bashrc"
+
 #export NVM_DIR="$HOME/.nvm"
 #[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 #[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
