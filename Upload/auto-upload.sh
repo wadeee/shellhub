75cbe40e1fa473e448df218001903ec53b3c4291
wade <wadechen@outlook.com>
2025-09-12 18:51:44 +0800

    update

---
+++ Upload/auto-upload.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+remote_host=
+remote_user="root"
+remote_port="22"
+ssh_key=~/.ssh/id_rsa
+
+showHelp() {
+  echo "Usage: sh ./inst.sh [OPTIONS]"
+  echo "Options:"
+  echo "  -h <ssh host>"
+  echo "  -p <ssh port>"
+  echo "  -u <ssh user>"
+  exit;
+}
+
+while getopts "h:p:u:" opt; do
+  case "$opt" in
+    h ) remote_host=$OPTARG ;;
+    p ) remote_port=$OPTARG ;;
+    u ) remote_user=$OPTARG ;;
+    ? ) showHelp ;;
+  esac
+done
+
+## upload
+scp -P "$remote_port" -i $ssh_key ./RD.zip "$remote_user"@"$remote_host":/root/zips/
