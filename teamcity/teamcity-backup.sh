0b3b174134fffa5ff221cc936c8e4ef227670982
wade <wadechen@outlook.com>
2025-10-23 16:09:57 +0800

    update

--- teamcity/teamcity-backup.sh
+++ teamcity/teamcity-backup.sh
@@ -7,4 +7,6 @@ cd /root/TeamCity || exit
 zip -r /root/backup/teamcity/TeamCity.zip ./*
 cd /root/buildAgent || exit
 zip -r /root/backup/teamcity/buildAgent.zip ./*
+cd /root/.BuildServer || exit
+zip -r /root/backup/teamcity/.BuildServer.zip ./*
 mysqldump -uroot -ppassword --disable-keys --create-options --skip-extended-insert --routines --events --triggers teamcity > /root/backup/teamcity/teamcity.sql
