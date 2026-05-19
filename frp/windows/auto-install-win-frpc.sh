28a5784df4ae6c99d387027b58680f8aa1122336
wade <wadechen@outlook.com>
2026-05-19 11:41:13 +0800

    update

--- frp/windows/auto-install-win-frpc.sh
+++ frp/windows/auto-install-win-frpc.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-unzip -o ./frp_0.68.1_windows_amd64.zip -d /C/
-cp ./frpc.bat C:/frp_0.68.1_windows_amd64/
-cp ./frpc.yaml C:/frp_0.68.1_windows_amd64/
 nssm stop frpc
 nssm remove frpc
-nssm install frpc
+rm -rf /C/frp_0.68.1_windows_amd64/
+unzip -o ./frp_0.68.1_windows_amd64.zip -d /C/
+cp ./frpc.bat /C/frp_0.68.1_windows_amd64/
+cp ./frpc.yaml /C/frp_0.68.1_windows_amd64/
+nssm install frpc "C:\\frp_0.68.1_windows_amd64\\frpc.bat"
 nssm start frpc
