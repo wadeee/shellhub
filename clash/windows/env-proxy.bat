6785a8ea91d3e998521bd3dc4b06ee4e357c7024
wade <wadechen@outlook.com>
2026-06-06 22:30:23 +0800

    update

--- clash/windows/env-proxy.bat
+++ clash/windows/env-proxy.bat
@@ -1,7 +1,10 @@
 @echo off
-powershell -Command "Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned -Force"
-powershell -Command "[System.Environment]::SetEnvironmentVariable('all_proxy', 'socks5h://localhost:7890', 'Machine')"
-powershell -Command "[System.Environment]::SetEnvironmentVariable('http_proxy', 'http://localhost:7890', 'Machine')"
-powershell -Command "[System.Environment]::SetEnvironmentVariable('https_proxy', 'http://localhost:7890', 'Machine')"
-powershell -Command "[System.Environment]::SetEnvironmentVariable('ftp_proxy', 'http://localhost:7890', 'Machine')"
+
+powershell -Command "Set-ExecutionPolicy -Scope CurrentUser RemoteSigned -Force"
+
+powershell -Command "[Environment]::SetEnvironmentVariable('http_proxy','http://127.0.0.1:7890','Machine')"
+powershell -Command "[Environment]::SetEnvironmentVariable('https_proxy','http://127.0.0.1:7890','Machine')"
+powershell -Command "[Environment]::SetEnvironmentVariable('all_proxy','socks5://127.0.0.1:7890','Machine')"
+
+echo Proxy configured.
 pause
